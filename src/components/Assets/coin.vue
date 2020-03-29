76cbe48153765814bed06b43e6e6d434ea2205cb
xuxin2019-f <1263092750@qq.com>
2020-03-29 18:22:37 +0800

    复制页面

--- src/components/Assets/coin.vue
+++ src/components/Assets/coin.vue
@@ -1,31 +1,110 @@
 <template>
   <div>
-      <el-card class="box-card">
+      <!-- 头部 -->
+      <el-card class="box-card1">
+       <div  class="header">
+        <span>币币账户</span>
+       </div>
+      </el-card>
+
+       <!--主题内容  -->
+      <div>
+          <el-card class="box-card2">
         <div slot="header" class="clearfix">
-          <span>币币账户</span>
+          <span>总资产折合：0.00000000 BTC ≈ 0.00 CNY</span>
         </div>
         <div class="text item">
-          <span>这里是内容</span>
+          <!-- 列表显示 -->
+          <el-table
+           :data="tableData"
+           style="width: 100%"
+           fit
+           highlight-current-row
+           @row-click='showdetail'
+          >
+           <el-table-column
+            prop="type"
+            label="币种"
+            width="250"
+            sortable>
+           </el-table-column>
+           <el-table-column
+            prop="have"
+            label="持有量"
+            width="300">
+           </el-table-column>
+            <el-table-column
+            prop="calculate"
+            label="BTC估值"
+            width="250">
+           </el-table-column>
+            <el-table-column
+            label="冻结"
+            width="250">
+            0.0000000000
+           </el-table-column>
+           <el-table-column
+            label="操作"
+            width="250">
+            <el-button type="text">充币</el-button>
+            <el-button type="text">提币</el-button>
+           </el-table-column>
+          </el-table>
         </div>
       </el-card>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+      return {
+          tableData:[
+              {
+                  type:'ETH',
+                  have:'1,000,000',
+                  calculate:'0.00000000 ≈ 0.00 CNY'
+              },
+              {
+                  type:'ETH',
+                  have:'1,000,000',
+                  calculate:'0.00000000 ≈ 0.00 CNY'
+              },
+              {
+                  type:'BTC',
+                  have:'1,000,000',
+                  calculate:'0.00000000 ≈ 0.00 CNY'
+              },
+              {
+                  type:'BTC',
+                  have:'1,000,000',
+                  calculate:'0.00000000 ≈ 0.00 CNY'
+              },
+              {
+                  type:'BTC',
+                  have:'1,000,000',
+                  calculate:'0.00000000 ≈ 0.00 CNY'
+              }
+          ]
+      }
+  }
 }
 </script>
 
 <style scoped>
-.text {
-    font-size: 14px;
-  }
 
   .item {
     margin-bottom: 18px;
   }
-
+  .clearfix span {
+      position: absolute;
+      left: 30px;
+      font-size: 18px;
+  }
+  .clearfix{
+      height: 30px;
+  }
   .clearfix:before,
   .clearfix:after {
     display: table;
@@ -34,8 +113,26 @@ export default {
   .clearfix:after {
     clear: both
   }
-
-  .box-card {
-    width: 480px;
+  .header {
+      font-size: 25px;
+      font-weight: bold;
+      position: absolute;
+      left: 30px;
+  }
+  .box-card1 {
+    position: absolute;
+    margin-top: 30px;
+    left: 50%;
+    transform:translateX(-50%) ;
+    width: 80%;
+    height: 10%;
+  }
+  .box-card2 {
+    position: absolute;
+    margin-top: 130px;
+    left: 50%;
+    transform:translateX(-50%) ;
+    width: 80%;
+    height: 90%;
   }
 </style>
