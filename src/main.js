968ca9c420fd7795a233ad269f4e915b21de30d7
xuxin2019-f <1263092750@qq.com>
2020-03-29 22:41:26 +0800

    币币账户以及配置了axios的拦截

--- src/main.js
+++ src/main.js
@@ -6,6 +6,38 @@ import './plugins/element.js'
 import "./assets/css/global.css"
 // 路由守卫
 import "./permission";
+import axios from 'axios'
+
+// 创建axios实例
+const service = axios.create({
+  baseURL:'回头再写',
+  withCredentials:true,//跨域时若要发送cookies需设置该选项
+  timeout:5000 //超时
+})
+
+// 请求拦截器
+service.interceptors.request.use(
+  config=>{
+    // 为请求头对象，添加token验证的Authorization字段
+    // 或者用vuex中的token，这个到时候再定
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+  },
+  error =>{
+    // 请求错误预处理
+    return Promise.reject(error)
+  }
+)
+
+// 响应拦截器
+service.interceptors.response.use(
+  // 这里可以通过自定义code判断响应状态，也可以通过HTTP状态码判断
+  response =>{
+    const res = response.data
+    // 现在先不写东西，只是过滤只剩下返回数据的data数据
+    return res
+  }
+)
 
 Vue.config.productionTip = false
 
